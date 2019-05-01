7f306d1f03a7fe64a30a8ba7c236a77c3ce52f1b
Marcus Aguirre <marcus.a.aguirre@gmail.com>
2019-05-01 17:20:52 -0500

    complete 2 out of 3 exercises

---
+++ src/main/java/com/codeup/blog/MathController.java
@@ -0,0 +1,38 @@
+package com.codeup.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+    @GetMapping("/add/{number}/and/{secondNumber}")
+    @ResponseBody
+    public String addition(@PathVariable int number, @PathVariable int secondNumber){
+        return number + " + " + secondNumber + " = " + (number + secondNumber);
+    }
+
+
+    @GetMapping("/subtract/{number}/from/{secondNumber}")
+    @ResponseBody
+    public String subtract(@PathVariable int number, @PathVariable int secondNumber){
+        return number + " - " + secondNumber + " = " + (secondNumber - number);
+    }
+
+
+    @GetMapping("/multiply/{number}/and/{secondNumber}")
+    @ResponseBody
+    public String multiply(@PathVariable int number, @PathVariable int secondNumber){
+        return number + " x " + secondNumber + " = " + (number * secondNumber);
+    }
+
+
+    @GetMapping("/divide/{number}/by/{secondNumber}")
+    @ResponseBody
+    public String divide(@PathVariable int number, @PathVariable int secondNumber){
+        return number + " / " + secondNumber + " = " + (number / secondNumber);
+    }
+
+}
